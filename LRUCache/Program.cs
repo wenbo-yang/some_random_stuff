3d9e27750dfce506cd993f9ae0cd683f67ecc649
Wenbo Yang <wenboya@microsoft.com>
2018-07-09 21:52:15 -0700

    test

---
+++ LRUCache/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace LRUCache
+{
+    class Program
+    {
+        // use linkedList
+        class LRUCache
+        {
+            int capacity;
+            LinkedList<int> cache;
+            Dictionary<int, LinkedListNode<int>> table;
+
+            LRUCache(int cap)
+            {
+                capacity = cap;
+                cache = new LinkedList<int>();
+                table = new Dictionary<int, LinkedListNode<int>>();
+            }
+
+            int Get(int key)
+            {
+                if (!table.ContainsKey(key))
+                {
+                    return -1;
+                }
+
+                // assign prev 
+                cache.AddFirst(table[key].Value);
+                // this is O(1) according to documentation 
+                cache.Remove(table[key]); 
+                // Move this key to the front of the cache
+                return cache.First.Value;
+            }
+
+            void Put(int key, int value)
+            {
+                // Key already exists
+                if (table.ContainsKey(key))
+                {
+                    // Update the value
+                    Get(key);
+                    table[key].Value = value;
+                    return;
+                }
+
+                // Reached the capacity, remove the oldest entry
+                if (cache.Count == capacity)
+                {
+                    table.Remove(key);
+                    cache.RemoveLast();
+                }
+
+                // Insert the entry to the front of the cache and update mapping.
+                cache.AddFirst(new LinkedListNode<int>(value));
+                table[key] = cache.First;
+            }
+
+        };
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- RegularExpressionMatching/Program.cs
+++ RegularExpressionMatching/Program.cs
@@ -8,8 +8,53 @@ namespace RegularExpressionMatching
 {
     class Program
     {
+        // not looking for partial match
+        public static bool MatchRegex(char[] text, char[] pattern)
+        {
+            var T = new bool[text.Length + 1, pattern.Length + 1];
+            T[0, 0] = true;
+
+            //Deals with patterns like a* or a*b* or a*b*c*
+            for (int i = 1; i < T.GetLength(1); i++)
+            {
+                if (pattern[i - 1] == '*')
+                {
+                    T[0, i] = T[0, i - 2];
+                }
+            }
+
+            for (int i = 1; i < T.GetLength(0); i++)
+            {
+                for (int j = 1; j < T.GetLength(1); j++)
+                {
+                    if (pattern[j - 1] == '.' || pattern[j - 1] == text[i - 1])
+                    {
+                        T[i, j] = T[i - 1, j - 1];
+                    }
+                    else if (pattern[j - 1] == '*')
+                    {
+                        T[i, j] = T[i, j - 2];
+
+                        if (pattern[j - 2] == '.' ||
+                            pattern[j - 2] == text[i - 1])
+                        {
+                            T[i, j] = T[i, j] || T[i - 1, j];
+                        }
+                    }
+                    else
+                    {
+                        T[i, j] = false;
+                    }
+                }
+            }
+            return T[text.Length, pattern.Length];
+        }
+
         static void Main(string[] args)
         {
+            var test = new int[2, 3];
+            var length = test.GetLength(0);
+            var res = matchRegex(new char[] { 'a', 'a', 'a', 'b' }, new char[] {'c', '*', 'a', '*', 'b' });
         }
     }
 }
